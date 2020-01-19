901d3ee2399117e745d19c004f3e5ba2cfa1446f
Cameron Darren <51996720+Artlist-creator@users.noreply.github.com>
2020-01-19 06:08:39 -0500

    Initial commit :fire:

---
+++ script.js
@@ -0,0 +1,89 @@
+const screen = document.querySelector(".display-screen");
+let buffer = "0",
+  tot = 0,
+  operator;
+
+function getData(value) {
+  if (isNaN(value)) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  displayTot();
+}
+
+function handleNumber(value) {
+  if (buffer !== "0") {
+    buffer += value;
+  } else {
+    if (value !== "0") {
+      buffer = value;
+    }
+  }
+}
+
+function handleSymbol(value) {
+  switch (value) {
+    case "C":
+      buffer = "0";
+      tot = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substr(0, buffer.length - 1);
+      }
+      break;
+    case "=":
+      if (tot === 0) {
+        // tot will be initialized when a operator is clicked
+        // No tot No calculate
+        buffer = buffer == "" ? 0 : buffer;
+        return;
+      }
+      getNewBuffer(tot, buffer);
+      operator = undefined;
+      break;
+    default:
+      if (operator === undefined) {
+        tot = parseInt(buffer);
+      } else {
+        tot = mathItUp[operator](tot, buffer === "" ? 0 : parseInt(buffer));
+      }
+      buffer = "";
+      operator = value;
+  }
+
+  function getNewBuffer(total, currentNumber) {
+    tot =
+      operator === undefined ? tot : mathItUp[operator](tot, parseInt(buffer));
+    buffer = tot;
+  }
+}
+
+const mathItUp = {
+  "+": function(x, y) {
+    return x + y;
+  },
+  "-": function(x, y) {
+    return x - y;
+  },
+  "×": function(x, y) {
+    return x * y;
+  },
+  "÷": function(x, y) {
+    return x / y;
+  }
+};
+
+function displayTot() {
+  screen.innerText = buffer;
+}
+
+document.querySelector(".cal-box").addEventListener("click", function(event) {
+  if (event.target.tagName === "BUTTON") {
+    getData(event.target.innerText);
+  }
+  event.stopPropagation();
+});
